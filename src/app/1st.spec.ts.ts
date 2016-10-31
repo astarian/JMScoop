0e76a2211a3fa94bfb2c2618974e9fc6b0509e43
Morgan Benton <registered@morphatic.com>
2016-10-31 14:24:24 -0400

    added first test

--- src/app/1st.spec.ts.ts
+++ src/app/1st.spec.ts.ts
@@ -1,6 +1,26 @@
-import { Component } from '@angular/core';
+import { ComponentFixture, async } from '@angular/core/testing';
+import { TestUtils }               from '../test';
+import { HomePage }          from '../pages/home/home';
+import { By }           from '@angular/platform-browser';
 
+let fixture: ComponentFixture<HomePage> = null;
+let instance: any = null;
 
-describe('1st tests', () => {
-  it('true is true', () => expect(true).toBe(true));
+describe('Pages: HelloIonic', () => {
+
+  beforeEach(async(() => TestUtils.beforeEachCompiler([HomePage]).then(compiled => {
+    fixture = compiled.fixture;
+    instance = compiled.instance;
+  })));
+
+  it('should create the hello ionic page', async(() => {
+    expect(instance).toBeTruthy();
+  }));
+
+  it('should have a button that says "I Need a Ride!"', async(() => {
+    fixture.detectChanges();
+    let inar = fixture.debugElement.query(By.css('#inar')).nativeElement;
+    //console.log(inar);
+    expect(inar.innerText).toMatch(/I Need a Ride!/);
+  }));
 });
